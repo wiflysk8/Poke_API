35b9c36f833fb49ac1ab758d68e3fad12893e087
Luis Martínez <100946370+wiflysk8@users.noreply.github.com>
2022-03-13 17:08:07 +0100

    add detail

--- script.js
+++ script.js
@@ -16,87 +16,182 @@ const getPokemons = async () => {
 
 const printPokemons = (pokemons) => {
   //funcion para pintar los pokemons
-  gallery$$.innerHTML = '';
+  gallery$$.innerHTML = "";
   for (let pokemon of pokemons) {
-    if(pokemon.name.toLowerCase().includes(input$$.value.toLowerCase()) ||  pokemon.types[0].type.name.toLowerCase().includes(input$$.value.toLowerCase())){
-    const figure$$ = document.createElement("figure");
-    const titulo$$ = document.createElement("h2");
-    const image$$ = document.createElement("img");
-    const stats$$ = document.createElement("div");
-    const type$$ = document.createElement("span");
-    const height$$ = document.createElement("span");
-    const weight$$ = document.createElement("span");
+    if (
+      pokemon.name.toLowerCase().includes(input$$.value.toLowerCase()) ||
+      pokemon.types[0].type.name
+        .toLowerCase()
+        .includes(input$$.value.toLowerCase())
+    ) {
+      const figure$$ = document.createElement("figure");
+      const titulo$$ = document.createElement("h2");
+      const image$$ = document.createElement("img");
+      const stats$$ = document.createElement("div");
+      const type$$ = document.createElement("span");
+      const height$$ = document.createElement("span");
+      const weight$$ = document.createElement("span");
 
-    titulo$$.textContent = pokemon.name.toUpperCase();
-    image$$.src = pokemon.sprites.other.home.front_default;
-    type$$.textContent = "Type: " + pokemon.types[0].type.name;
-    height$$.textContent = "Height: " + pokemon.height * 10 + "cm";
-    weight$$.textContent = "Weight: " + pokemon.weight / 10 + "kg";
+      titulo$$.textContent = pokemon.name.toUpperCase();
+      image$$.src = pokemon.sprites.other.home.front_default;
+      type$$.textContent = "Type: " + pokemon.types[0].type.name;
+      height$$.textContent = "Height: " + pokemon.height * 10 + "cm";
+      weight$$.textContent = "Weight: " + pokemon.weight / 10 + "kg";
 
-    switch (pokemon.types[0].type.name) {
-      case 'water':
-      figure$$.className = 'water';
-      break;
-      case 'fire':
-      figure$$.className = 'fire';
-      break;
-      case 'grass':
-      figure$$.className = 'grass';
-      break;
-      case 'bug':
-      figure$$.className = 'bug';
-      break;
-      case 'poison':
-      figure$$.className = 'poison';
-      break;
-      case 'electric':
-      figure$$.className = 'electric';
-      break;
-      case 'fighting':
-      figure$$.className = 'fighting';
-      break;
-      case 'psychic':
-      figure$$.className = 'psychic';
-      break;
-      case 'fairy':
-      figure$$.className = 'fairy';
-      break;
-      case 'rock':
-      figure$$.className = 'rock';
-      break;
-      case 'ground':
-      figure$$.className = 'ground';
-      break;
-      case 'normal':
-      figure$$.className = 'normal';
-      break;
-      case 'ghost':
-      figure$$.className = 'ghost';
-      break;
-      case 'ice':
-      figure$$.className = 'ice';
-      break;
-      case 'dragon':
-      figure$$.className = 'dragon';
-      break;
+      switch (pokemon.types[0].type.name) {
+        case "water":
+          figure$$.className = "b-card b-card__water";
+          break;
+        case "fire":
+          figure$$.className = "b-card b-card__fire";
+          break;
+        case "grass":
+          figure$$.className = "b-card b-card__grass";
+          break;
+        case "bug":
+          figure$$.className = "b-card b-card__bug";
+          break;
+        case "poison":
+          figure$$.className = "b-card b-card__poison";
+          break;
+        case "electric":
+          figure$$.className = "b-card b-card__electric";
+          break;
+        case "fighting":
+          figure$$.className = "b-card b-card__fighting";
+          break;
+        case "psychic":
+          figure$$.className = "b-card b-card__psychic";
+          break;
+        case "fairy":
+          figure$$.className = "b-card b-card__fairy";
+          break;
+        case "rock":
+          figure$$.className = "b-card b-card__rock";
+          break;
+        case "ground":
+          figure$$.className = "b-card b-card__ground";
+          break;
+        case "normal":
+          figure$$.className = "b-card b-card__normal";
+          break;
+        case "ghost":
+          figure$$.className = "b-card b-card__ghost";
+          break;
+        case "ice":
+          figure$$.className = "b-card b-card__ice";
+          break;
+        case "dragon":
+          figure$$.className = "b-card b-card__dragon";
+          break;
+      }
+
+      figure$$.appendChild(titulo$$);
+      figure$$.appendChild(image$$);
+      stats$$.appendChild(type$$);
+      stats$$.appendChild(height$$);
+      stats$$.appendChild(weight$$);
+      figure$$.appendChild(stats$$);
+      gallery$$.appendChild(figure$$);
+
+      image$$.addEventListener("click", () => pokemonDetail(pokemon));
     }
-
-    figure$$.appendChild(titulo$$);
-    figure$$.appendChild(image$$);
-    stats$$.appendChild(type$$);
-    stats$$.appendChild(height$$);
-    stats$$.appendChild(weight$$);
-    figure$$.appendChild(stats$$);
-    gallery$$.appendChild(figure$$);
   }
-}console.log(input$$.value);
+  console.log(input$$.value);
 };
 
-const input$$ = document.querySelector('input');
-input$$.addEventListener('input', () => printPokemons(pokemons))
+const input$$ = document.querySelector("input");
+input$$.addEventListener("input", () => printPokemons(pokemons));
 console.log(input$$.value);
 
+const pokemonDetail = (pokemon) => {
+  gallery$$.innerHTML = "";
 
+  const figure$$ = document.createElement("figure");
+  figure$$.className = ('b-card');
+  const h2Div$$ = document.createElement('div');
+  h2Div$$.className = ('b-card-title')
+  const titulo$$ = document.createElement("h2");
+  const image$$ = document.createElement("img");
+  const stats$$ = document.createElement("div");
+  const type$$ = document.createElement("span");
+  const height$$ = document.createElement("span");
+  const weight$$ = document.createElement("span");
+  
+
+  titulo$$.textContent = pokemon.name.toUpperCase();
+  image$$.src = pokemon.sprites.other.dream_world.front_default;
+  type$$.textContent = "Type: " + pokemon.types[0].type.name;
+  height$$.textContent = "Height: " + pokemon.height * 10 + "cm";
+  weight$$.textContent = "Weight: " + pokemon.weight / 10 + "kg";
+
+  h2Div$$.appendChild(titulo$$);
+  figure$$.appendChild(h2Div$$);
+  figure$$.appendChild(image$$);
+  stats$$.appendChild(type$$);
+  stats$$.appendChild(height$$);
+  stats$$.appendChild(weight$$);
+  figure$$.appendChild(stats$$);
+  
+  const backBtn$$ = document.createElement('button');
+  backBtn$$.className = ('b-btn-back')
+  backBtn$$.textContent = "Go Back";
+  backBtn$$.addEventListener("click", function (event) {
+    location.reload();
+  });
+
+  figure$$.appendChild(backBtn$$);
+  gallery$$.appendChild(figure$$);
+
+  switch (pokemon.types[0].type.name) {
+    case "water":
+      figure$$.className = "b-card-detail b-card-detail__water";
+      break;
+    case "fire":
+      figure$$.className = "b-card-detail b-card-detail__fire";
+      break;
+    case "grass":
+      figure$$.className = "b-card-detail b-card-detail__grass";
+      break;
+    case "bug":
+      figure$$.className = "b-card-detail b-card-detail__bug";
+      break;
+    case "poison":
+      figure$$.className = "b-card-detail b-card-detail__poison";
+      break;
+    case "electric":
+      figure$$.className = "b-card-detail b-card-detail__electric";
+      break;
+    case "fighting":
+      figure$$.className = "b-card-detail b-card-detail__fighting";
+      break;
+    case "psychic":
+      figure$$.className = "b-card-detail b-card-detail__psychic";
+      break;
+    case "fairy":
+      figure$$.className = "b-card-detail b-card-detail__fairy";
+      break;
+    case "rock":
+      figure$$.className = "b-card-detail b-card-detail__rock";
+      break;
+    case "ground":
+      figure$$.className = "b-card-detail b-card-detail__ground";
+      break;
+    case "normal":
+      figure$$.className = "b-card-detail b-card-detail__normal";
+      break;
+    case "ghost":
+      figure$$.className = "b-card-detail b-card-detail__ghost";
+      break;
+    case "ice":
+      figure$$.className = "b-card-detail b-card-detail__ice";
+      break;
+    case "dragon":
+      figure$$.className = "b-card-detail b-card-detail__dragon";
+      break;
+  }
+
+  console.log(pokemon.name);
+};
 
 getPokemons();
-
